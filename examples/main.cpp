f0363d8c4bbc3bba557a45162e1e069b686587a7
eltoshan <eltoshan@gmail.com>
2020-02-03 13:16:53 -0500

    add avx2 example

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+#include <immintrin.h>
+
 #include "hnsw/hnsw.h"
 
 
@@ -20,10 +22,39 @@ static float sim_func(data_t& a, data_t& b, size_t n) {
     return -res;
 }
 
+float hsum_ps_sse3(__m128 v) {
+    __m128 shuf = _mm_movehdup_ps(v);        // broadcast elements 3,1 to 2,0
+    __m128 sums = _mm_add_ps(v, shuf);
+    shuf        = _mm_movehl_ps(shuf, sums); // high half -> low half
+    sums        = _mm_add_ss(sums, shuf);
+    return        _mm_cvtss_f32(sums);
+}
+
+float hsum256_ps_avx(__m256 v) {
+    __m128 vlow  = _mm256_castps256_ps128(v);
+    __m128 vhigh = _mm256_extractf128_ps(v, 1); // high 128
+           vlow  = _mm_add_ps(vlow, vhigh);     // add the low 128
+    return hsum_ps_sse3(vlow);         // and inline the sse3 version, which is optimal for AVX
+    // (no wasted instructions, and all of them are the 4B minimum)
+}
+
+static float sim_func_avx(data_t& a, data_t& b, size_t n) {
+    float res;
+    for (size_t i = 0; i < n; i += 8) {
+        __m256 vec_a = _mm256_loadu_ps(&a[i]);
+        __m256 vec_b = _mm256_loadu_ps(&b[i]);
+        __m256 vec_t = _mm256_sub_ps(vec_a, vec_b);
+        __m256 vec_t2 = _mm256_mul_ps(vec_t, vec_t);
+        res += hsum256_ps_avx(vec_t2);
+    }
+    return -res;
+} 
+
 int main() {
 
-    hnsw::METRICFUNC<sim_t,data_t> mfunc = sim_func;
-    size_t data_dim = 5;
+    // hnsw::METRICFUNC<sim_t,data_t> mfunc = sim_func;
+    hnsw::METRICFUNC<sim_t,data_t> mfunc = sim_func_avx;
+    size_t data_dim = 16;
 
     hnsw::Index<sim_t,data_t,node_t> index (mfunc, data_dim, 5, 10);
 
